42fffcb8eee6a2be12684d584e151236281001b7
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-21 17:38:29 +0200

    add user & dashboard components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { UserComponent } from './services/components/user/user.component';
+import { DashboardComponent } from './services/components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatToolbarModule, MatSidenavModule, MatIconModule, UserComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { DashboardComponent } from '../app/services/components/dashboard/dashboard.component';
+import { UserComponent } from '../app/services/components/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DashboardComponent },
+    {path: 'dashboard', component: DashboardComponent },
+    {path: 'user', component: UserComponent },
+];
